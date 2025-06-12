6f4302d8ffd37d6c1790b481111bebb392311318
Faareh-Ahmed <faarehahmed@gmail.com>
2025-06-13 00:20:00 +0500

    Updated Packages for Deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Galvan-AI/',
   plugins: [
     tailwindcss(),
     react()
